410cbccead56f8865e48b8dd4966dc438c0b7ffc
Mark Attar <124407382+markattarcolgate64@users.noreply.github.com>
2024-02-17 14:25:33 -0500

    initial changes to structure
    Changing structure and removing unwanted elements in header and other places

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,7 +15,7 @@ const AboutPage = () => {
     <>
       <Breadcrumb
         pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        description="This is a Mergers and Acquisitions solutions startup that leverages technologies."
       />
       <AboutSectionOne />
       <AboutSectionTwo />
